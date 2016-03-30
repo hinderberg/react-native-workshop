5935d30f172de4458d71b5c40155c31d30f07e9e
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2016-03-30 19:36:15 +0200

    Fjerner ubrukt styling

--- app/components/navigation/navigationbar-buttons.android.js
+++ app/components/navigation/navigationbar-buttons.android.js
@@ -14,10 +14,6 @@ const styles = {
         right: 0,
         height: 50
     },
-    navBarRightText: {
-        top: -3,
-        color: '#ffffff'
-    },
     sendButton: {
       padding: 15,
       top: -3
