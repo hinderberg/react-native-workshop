1d03f103bc03e35d1d35e573d8b6b7235dc7cb3b
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2016-03-30 21:04:50 +0200

    Fjernet kode for oppgave 1

--- app/components/navigation/navigationbar-buttons.android.js
+++ app/components/navigation/navigationbar-buttons.android.js
@@ -3,32 +3,10 @@ import React, { TouchableOpacity } from 'react-native';
 import { newMessage } from './routes';
 import ConnectedSendButton from './spinning-send-button-DO-NOT-TOUCH';
 
-const styles = {
-    navBarRightButton: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        top: 0,
-        right: 0,
-        height: 50
-    },
-    sendButton: {
-      padding: 15,
-      top: -3
-    }
-};
-
 const navigationBarButtons = (dispatch) => ({
 
     RightButton (route) {
-        if (route.title === newMessage.title) {
-            return (
-                <TouchableOpacity style={styles.navBarRightButton}>
-                  <ConnectedSendButton style={styles.sendButton} />
-                </TouchableOpacity>
-            );
-        }
-        return null;
+
     }
 });
 
--- app/components/new-message.js
+++ app/components/new-message.js
@@ -17,64 +17,14 @@ import { postMessage, setNewMessageText, setUsername, openImagePicker, clearImag
 class NewMessage extends Component {
     render() {
         const { username, newMessageText, setNewMessageText, setUsername, error } = this.props;
-        const sendButton = Platform.OS === 'android' ? null : this._renderSendButton();
-        const feedback = error ?
-            <View style={styles.feedback}><Text style={styles.feedbackText}>{error}</Text></View> : null;
 
+        // Hint React-Native uses Controlled Components https://facebook.github.io/react/docs/forms.html#controlled-components
         return (
             <View style={styles.container}>
-                <View style={styles.inputElements}>
-                    <View style={styles.inputContainer}>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Navn"
-                            placeholderTextColor={Colors.Dark1}
-                            underlineColorAndroid={Colors.White}
-                            value={username}
-                            onChangeText={setUsername}
-                        />
-                    </View>
-
-                    <View style={styles.inputContainer}>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Melding"
-                            placeholderTextColor={Colors.Dark1}
-                            underlineColorAndroid={Colors.White}
-                            value={newMessageText}
-                            onChangeText={setNewMessageText}
-                        />
-                    </View>
-                </View>
-                { feedback }
-                { sendButton }
+                <Text>New Message View</Text>
             </View>
         );
     }
-
-    _renderSendButton() {
-        return (
-            <TouchableOpacity
-                style={[styles.button, {backgroundColor: Colors.Red}]}
-                onPress={this.props.postMessage}>
-                <Text style={styles.buttonText}>SEND</Text>
-            </TouchableOpacity>
-        );
-    }
-}
-
-function inputContainerStyle() {
-    const base = {
-        margin: 10,
-        padding: 5
-    };
-    const ios = {
-        height: 35,
-        borderBottomWidth: 1,
-        borderBottomColor: Colors.White
-    };
-
-    return Platform.OS === 'android' ? base : {...base, ...ios};
 }
 
 const styles = StyleSheet.create({
@@ -82,35 +32,6 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 20,
         marginTop: 60
-    },
-    inputElements: {
-        backgroundColor: Colors.Dark,
-        paddingTop: Platform.OS === 'android' ? 0 : 20,
-        paddingBottom: Platform.OS === 'android' ? 0 : 20,
-        marginBottom: 8
-    },
-    inputContainer: inputContainerStyle(),
-    input: {
-        flex: 1,
-        color: Colors.White,
-        fontFamily: Fonts.Light
-    },
-    feedback: {
-        paddingLeft: 10,
-        marginTop: -2,
-        marginBottom: 8
-    },
-    feedbackText: {
-        color: Colors.Red,
-        fontFamily: Fonts.Demi
-    },
-    button: {
-        padding: 10,
-        alignItems: 'center'
-    },
-    buttonText: {
-        color: Colors.White,
-        fontFamily: Fonts.Light
     }
 });
 
